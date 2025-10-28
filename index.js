f70be81e6be9021d1a60979ac1dcdffdc33c2117
DragonCode <151834618+Whitedragon115@users.noreply.github.com>
2025-10-29 07:34:11 +0800

    add api server

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 const { Client } = require("@notionhq/client");
-const express = require('express');
-const request = require('request');
+const fs = require('fs');
 require('dotenv').config();
 
+const logger = require('./tool/log.js');
 const notion = new Client({ auth: process.env.NOTION_TOKEN });
 module.exports = {
-    notion
+    notion,
 }
+
+logger.box('Initializing Notion Blogger Application', 'info');
+checkFiles();
 
 const { writeConfig, readConfig } = require('./tool/json.js');
 const { getAllPage } = require('./function/getBlogPage.js');
 const { getMarkdown } = require('./function/getPageContent.js');
-const logger = require('./tool/log.js');
+const { express, app, load: loadAPI } = require('./api/server.js');
+
+async function init(){
+    await apiServer();
+}
+
+async function apiServer(){
+    logger.box('Starting API Server', 'info');
+
+    const port = process.env.PORT || 7630;
+    const apiRouter = express.Router();
+    loadAPI(apiRouter);
+    app.use('/api', apiRouter);
+
+    app.listen(port, () => {
+        logger.success(`API Server is running at http://localhost:${port}`);
+    });
+}
 
 async function db_source(){
-    logger.line();
     logger.box('Notion Database Source Configuration', 'info');
     logger.info('Initializing Notion Database Source Configuration...');
 
@@ -36,11 +55,37 @@ async function db_source(){
     logger.success('Configuration written to config.json successfully!');
 }
 
-const fs = require('fs');
+async function checkFiles(){
+    const defaultSyntax = [{'json': '{}'}]
 
-(async () => {
+    const checkFiles = ['./config.json']
+    const checkFolders = ['./markdown']
+    
+    logger.info('Checking files...')
+    for(const file of checkFiles){
+        if(!fs.existsSync(file)){
+            logger.warn(`File ${file} not found. Created a new one.`);
+            const subfix = file.split('.').pop()
+            const syntax = defaultSyntax.find(item => Object.keys(item)[0] === subfix)?.[subfix] ?? ''
+            fs.writeFileSync(file, syntax);
+            logger.success(`File ${file} created successfully.`);
+        }else{
+            logger.info(`File ${file} exists.`);
+        }
+    }
 
-    const data = await getMarkdown('29716938-eb37-80be-9efa-d9454747f8dc');
-    fs.writeFileSync('output.md', data.parent);
+    logger.info('Checking folders...')
+    for(const folder of checkFolders){
+        if(!fs.existsSync(folder)){
+            logger.warn(`Folder ${folder} not found. Created a new one.`);
+            fs.mkdirSync(folder);
+            logger.success(`Folder ${folder} created successfully.`);
+        }else{
+            logger.info(`Folder ${folder} exists.`);
+        }
+    }
 
-})();
+    logger.success('File and folder check completed.');
+}
+
+init();
