e6cd6ab53d2c65785351e91411ab76a146b243cd
DragonCode <151834618+Whitedragon115@users.noreply.github.com>
2025-10-29 09:17:29 +0800

    add function

--- index.js
+++ index.js
@@ -3,45 +3,61 @@ const fs = require('fs');
 require('dotenv').config();
 
 const logger = require('./tool/log.js');
-const notion = new Client({ auth: process.env.NOTION_TOKEN });
-module.exports = {
-    notion,
-}
+const silentLogger = { debug: () => { }, info: () => { }, warn: () => { }, error: () => { }, };
+
+const notion = new Client({
+    auth: process.env.NOTION_TOKEN,
+    logger: silentLogger,
+});
+
+module.exports = { notion }
 
 logger.box('Initializing Notion Blogger Application', 'info');
 checkFiles();
 
 const { writeConfig, readConfig } = require('./tool/json.js');
-const { getAllPage } = require('./function/getBlogPage.js');
-const { getMarkdown } = require('./function/getPageContent.js');
+const { getMarkdown, getBlogPage } = require('./function/notion/page.js');
 const { express, app, load: loadAPI } = require('./api/server.js');
 
-async function init(){
+async function init() {
+    await cleanTemp();
     await apiServer();
+
+    logger.box('Server fully started', 'success');
 }
 
-async function apiServer(){
+async function apiServer() {
     logger.box('Starting API Server', 'info');
 
     const port = process.env.PORT || 7630;
     const apiRouter = express.Router();
     loadAPI(apiRouter);
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
     app.use('/api', apiRouter);
 
-    app.listen(port, () => {
-        logger.success(`API Server is running at http://localhost:${port}`);
+    return new Promise((resolve, reject) => {
+        const server = app.listen(port, () => {
+            logger.success(`API Server is running in local using port ${port}`);
+            logger.success('Public URL is ' + (process.env.BASE_URL || `http://localhost:${port}`));
+            resolve(server);
+        });
+        server.on('error', (err) => {
+            logger.error(`Failed to start API Server: ${err.message}`);
+            reject(err);
+        });
     });
 }
 
-async function db_source(){
+async function db_source() {
     logger.box('Notion Database Source Configuration', 'info');
     logger.info('Initializing Notion Database Source Configuration...');
 
     const databaseId = process.env.DATABASE_ID;
-    
+
     logger.info(`Using Database ID: ${databaseId}`);
     logger.info('Fetching Data Source ID from Notion API...');
-    
+
     const dataSourceId = await notion.databases.retrieve({ database_id: databaseId }).then(res => {
         return res.data_sources[0].id;
     });
@@ -55,32 +71,49 @@ async function db_source(){
     logger.success('Configuration written to config.json successfully!');
 }
 
-async function checkFiles(){
-    const defaultSyntax = [{'json': '{}'}]
+async function cleanTemp(){
+    logger.box('Cleaning Temporary Files', 'info');
+    const tempFolder = './markdown';
+    let count = 0;
+
+    logger.info('Cleaning temporary files...');
+    if (fs.existsSync(tempFolder)) {
+        const files = fs.readdirSync(tempFolder);
+        files.forEach(file => {
+            fs.unlinkSync(`${tempFolder}/${file}`);
+            count++;
+        });
+    }
+
+    logger.success(`Temporary files cleaned successfully! Deleted ${count} files.`);
+}
+
+async function checkFiles() {
+    const defaultSyntax = [{ 'json': '{}' }]
 
     const checkFiles = ['./config.json']
     const checkFolders = ['./markdown']
-    
+
     logger.info('Checking files...')
-    for(const file of checkFiles){
-        if(!fs.existsSync(file)){
+    for (const file of checkFiles) {
+        if (!fs.existsSync(file)) {
             logger.warn(`File ${file} not found. Created a new one.`);
             const subfix = file.split('.').pop()
             const syntax = defaultSyntax.find(item => Object.keys(item)[0] === subfix)?.[subfix] ?? ''
             fs.writeFileSync(file, syntax);
             logger.success(`File ${file} created successfully.`);
-        }else{
+        } else {
             logger.info(`File ${file} exists.`);
         }
     }
 
     logger.info('Checking folders...')
-    for(const folder of checkFolders){
-        if(!fs.existsSync(folder)){
+    for (const folder of checkFolders) {
+        if (!fs.existsSync(folder)) {
             logger.warn(`Folder ${folder} not found. Created a new one.`);
             fs.mkdirSync(folder);
             logger.success(`Folder ${folder} created successfully.`);
-        }else{
+        } else {
             logger.info(`Folder ${folder} exists.`);
         }
     }
